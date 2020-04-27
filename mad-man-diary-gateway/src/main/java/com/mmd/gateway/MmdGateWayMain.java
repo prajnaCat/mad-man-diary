586c11d7f75ea66108342b62d32af4aa99d8d5fb
changhui.xie <changhui.xie@kingstarfintech.com>
2020-04-27 08:50:24 +0800

    增加book/gateway/user/writer

---
+++ mad-man-diary-gateway/src/main/java/com/mmd/gateway/MmdGateWayMain.java
@@ -0,0 +1,18 @@
+package com.mmd.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author xch
+ * @description 网关启动类
+ * @date 2020-04-26
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class MmdGateWayMain {
+    public static void main(String[] args) {
+        SpringApplication.run(MmdGateWayMain.class, args);
+    }
+}
